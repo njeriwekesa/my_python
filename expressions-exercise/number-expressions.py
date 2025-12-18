7b3551d939ce349fa959d27aa829527b9da1c055
Charity Njeri <wekesacharity@gmail.com>
2025-12-18 15:35:44 +0300

    Add number expressions.py

---
+++ expressions-exercise/number-expressions.py
@@ -0,0 +1,21 @@
+print(2 + 3) #5
+print(10 - 15) # -5
+print(4 + 1 - 5) #0
+print(4 * 3) #12
+
+print(7 / 2) #3.5
+print(4 + 2 * 3) #10
+print((4 + 2) * 3) #18
+print(5 % 2) #1
+
+print(6 % 2) #0
+print(7 % 2) #1
+print(8 % 2) #0
+print(19 % 8) #3
+
+print(24 % 8) #0
+print(7 % 4) #4  remember: a % b = a   (when a < b)
+print(4 % 7) #0
+print(5 + (10 % 15)) #5
+print((5 + 10) % 5) #0
+
