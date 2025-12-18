2da62c16efb3fd7379fc1c4311ca1eb8b446bed7
Charity Njeri <wekesacharity@gmail.com>
2025-12-18 12:06:06 +0300

    Add error handling for division by zero

---
+++ errors_and_exceptions/ZeroDivisionError.py
@@ -0,0 +1,11 @@
+# Exercise 1: Handling ZeroDivisionError
+
+# Instructions:
+
+# Write a program that takes two numbers as input from the user and divides the first number by the second number.
+# Handle the ZeroDivisionError exception to inform the user if they attempt to divide by zero.
+
+
+def division(num1, num2):
+  division = int(input("Enter num1", "Enter num2"))
+  print(num1 / num2)
