bb4c96c42ca69e3fa0d41431094fd7d53e4a7f31
Charity Njeri <wekesacharity@gmail.com>
2025-12-18 15:01:52 +0300

    Add string epressions

---
+++ expressions-exercise/string-expressions.py
@@ -0,0 +1,5 @@
+print("river" + "town") # rivertown
+print("cat" + "dog") #catdog
+print("New" + "York") #NewYork
+print("runner's knee" + "!") #runner's knee!
+print ("man" +"bear" +"pig") #manbearpig
